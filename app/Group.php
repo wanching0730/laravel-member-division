a56f6e835c94b2588eef7335863032eab29bc5a1
wanching_97 <wanching_97@hotmail.com>
2018-03-28 01:43:46 +0800

    added custom name for pivot table

--- app/Group.php
+++ app/Group.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
+
+    //protected $table = 'group';
+    
     public function members() 
     {
-        return $this->belongsToMany(Member::class);
+        return $this->belongsToMany(Member::class, 'member_group_rel');
     }
 }
